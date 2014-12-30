8eeedc94fd3cac6fa13ca2e1c3d15c0b1ec6e9e4
Brian Ball <btball@gmail.com>
2014-12-30 13:35:59 -0800

    Fixed version

---
+++ Adv660/version.h
@@ -0,0 +1,12 @@
+//
+//  version.h
+//  Adv660
+//
+//  Created by Brian Ball on 12/29/14.
+//  Copyright (c) 2014 Brian. All rights reserved.
+//
+
+#ifndef Adv660_version_h
+#define Adv660_version_h
+char * adv_version = "_START_TEXT_adv660";
+#endif
